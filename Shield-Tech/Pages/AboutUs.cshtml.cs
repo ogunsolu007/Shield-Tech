b78edcd88a98c47ead9e33ad1f6fa3c2eea15de8
wale <wale@DESKTOP-RRTOTM9>
2024-03-03 18:01:51 +0000

    Add project files.

---
+++ Shield-Tech/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shield_Tech.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Shield-Tech/Pages/Store.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shield_Tech.Pages
+{
+    public class StoreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

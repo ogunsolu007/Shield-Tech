523e4757edcc56ee4b078b09ce0b187d3d1289b7
wale <wale@DESKTOP-RRTOTM9>
2024-03-10 16:19:00 +0000

    user filtering product

--- Shield-Tech/Pages/Store.cshtml.cs
+++ Shield-Tech/Pages/Store.cshtml.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Shield_Tech.Models;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 
 namespace Shield_Tech.Pages
 {
     public class StoreModel : PageModel
     {
         public readonly AppDataContext _db;
-        public List<Product> Products { get; set; }
+
 
         public StoreModel(AppDataContext db)
         {
@@ -17,13 +20,42 @@ namespace Shield_Tech.Pages
         }
 
 
+        public IList<Product> Product { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Categories { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? ProductCategory { get; set; }
+
+
 
         public void OnGet()
         {
-            Products = _db.Products.ToList();
+            IQueryable<string> productQuery = from m in _db.Products
+                                              orderby m.Category
+                                              select m.Category;
+            var product = from m in _db.Products
+                          select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                product = product.Where(s => s.Productdescription.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(ProductCategory))
+            {
+                product = product.Where(x => x.Category == ProductCategory);
+
+            }
+
+
+
+            Categories = new SelectList(productQuery.Distinct().ToList());
+
+            Product = product.ToList();
         }
-
-
-
     }
 }
+
+
